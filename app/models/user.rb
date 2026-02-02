bdd1d23a7206d964035ca78f39e539a133aeb2f8
yungProg <22945393+yungProg@users.noreply.github.com>
2026-02-02 20:47:23 +0000

    user can sign in/up using username and/or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
+  attr_writer :login
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :username, presence: true, uniqueness: { case_sensitive: false }
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, multiline: true
 
   has_many :follower_relationships, class_name: "UserFollowing", foreign_key: :following_id
   has_many :following_relationships, class_name: "UserFollowing", foreign_key: :follower_id
@@ -17,4 +20,18 @@ class User < ApplicationRecord
   has_one :profile
 
   scope :other_users, ->(uid) { where.not("id = ?", uid) }
+
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if (login = conditions.delete(:login))
+      where(conditions.to_h).where([ "lower(username) = :value OR lower(email) = :value", { value: login.downcase } ]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      conditions[:email].downcase! if conditions[:email]
+      where(conditions.to_h).first
+    end
+  end
 end
